de4d13e78f3a4cd9f2d0478540700b1c9ea656a3
Erin Gallagher <erin.catherine.gallagher@gmail.com>
2026-06-14 21:42:50 +0100

    xcode project setup

---
+++ GoogleCalNoisyNotification/GoogleCalNoisyNotification/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GoogleCalNoisyNotification
+//
+//  Created by Erin Gallagher on 2026-06-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GoogleCalNoisyNotification/GoogleCalNoisyNotification/GoogleCalNoisyNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleCalNoisyNotificationApp.swift
+//  GoogleCalNoisyNotification
+//
+//  Created by Erin Gallagher on 2026-06-14.
+//
+
+import SwiftUI
+
+@main
+struct GoogleCalNoisyNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
